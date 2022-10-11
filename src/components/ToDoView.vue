f550487b889fa46a9229c58b7b155ef47007bdea
camilorendon <juancamilo.g.0512@gmail.com>
2022-10-11 15:56:54 -0500

    new component

--- src/components/ToDoView.vue
+++ src/components/ToDoView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {reactive} from 'vue'
+import InputNew from './InputNew.vue';
 
 const boards = reactive([
     {
@@ -33,6 +34,11 @@ const boards = reactive([
     }
 ]);
 
+
+function handleNewItem(text){
+    console.log(text.value)
+}
+
 </script>
 
 <template>
@@ -47,6 +53,7 @@ const boards = reactive([
         <div class="boards">
             <div class="board" v-for="board in boards" :key="board.id">
                 <div> {{board.name}} </div>
+                <InputNew @on-new-item="handleNewItem"/>
                 <div class="items">
                     <div class="item" v-for="item in board.items" :key="item.id"> {{item.title}} </div>
                 </div>
@@ -56,6 +63,26 @@ const boards = reactive([
 </template>
 
 <style scoped>
+
+.boards{
+    display: flex;
+    gap: 10px;
+}
+.board{
+    background: #efefef;
+    padding: 10px;
+}
+.items{
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+}
+.item{
+    background-color: white;
+    padding: 10px;
+    box-sizing: border-box;
+}
+
 
 </style>
 
